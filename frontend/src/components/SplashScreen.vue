b0d16ef272298ad35fe0724aeb5533d9d376d087
Prathamesh Shenoy <shenoyprathamesh123@gmail.com>
2019-01-20 23:13:49 -0600

    created comp. for splash screen & imported Logo

---
+++ frontend/src/components/SplashScreen.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="splash-screen">
+    <logo></logo>
+    <h2>A buffer to store your knowledge while it gets downloaded by others</h2>
+  </div>
+</template>
+
+<script>
+import Logo from '@/components/Logo'
+
+export default {
+  name: 'SplashScreen',
+  components: {
+    'logo': Logo
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
