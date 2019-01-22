4bee475d012402d42b6155891ed7dac6d711f89c
Prathamesh Shenoy <shenoyprathamesh123@gmail.com>
2019-01-21 18:59:08 -0600

    reorganized imports in splash screen test

--- frontend/tests/unit/components/SplashScreen.spec.js
+++ frontend/tests/unit/components/SplashScreen.spec.js
@@ -1,8 +1,8 @@
-import Logo from '@/components/Logo'
-import SplashScreen from '@/components/SplashScreen'
-
 import { shallowMount } from '@vue/test-utils'
 import { expect } from 'chai'
+
+import Logo from '@/components/Logo'
+import SplashScreen from '@/components/SplashScreen'
 
 describe('SplashScreen.vue', () => {
   const wrapper = shallowMount(SplashScreen)
