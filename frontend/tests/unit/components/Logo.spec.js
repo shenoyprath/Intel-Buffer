0adc1385ae93b4db4f80bf6a28dcd84dc038dca7
Prathamesh Shenoy <shenoyprathamesh123@gmail.com>
2019-01-21 18:58:21 -0600

    reorganized imports in logo test

--- frontend/tests/unit/components/Logo.spec.js
+++ frontend/tests/unit/components/Logo.spec.js
@@ -1,7 +1,7 @@
-import Logo from '@/components/Logo'
-
 import { shallowMount } from '@vue/test-utils'
 import { expect } from 'chai'
+
+import Logo from '@/components/Logo'
 
 describe('Logo.vue', () => {
   const wrapper = shallowMount(Logo)
