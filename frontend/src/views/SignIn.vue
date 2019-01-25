ddecc08f0ff81c790fdb91aaf525478058b39168
Prathamesh Shenoy <shenoyprathamesh123@gmail.com>
2019-01-24 22:37:08 -0600

    added markup for sign in page

--- frontend/src/views/SignIn.vue
+++ frontend/src/views/SignIn.vue
@@ -1,11 +1,28 @@
 <template>
   <div class="sign-in">
+    <div class="sign-in-modal">
+      <logo height="25pt"></logo>
+      <h2>Sign in to Continue</h2>
+
+      <form>
+        <input id="email" type="email" placeholder="Email Address" autofocus>
+        <input id="password" type="password" placeholder="Password">
+      </form>
+
+      <router-link to="/">Forgot Password?</router-link>
+      <button>Sign In</button>
+      <p>Don't have an account? <router-link to="/">Register</router-link></p>
+    </div>
   </div>
 </template>
 
 <script>
+import Logo from '@/components/Logo'
 export default {
-  name: 'SignIn'
+  name: 'SignIn',
+  components: {
+    'logo': Logo
+  }
 }
 </script>
 
