71d76130f74bb6ea4f714daaa082a50b5b562f07
Prathamesh Shenoy <shenoyprathamesh123@gmail.com>
2019-01-24 23:53:11 -0600

    test logo to check if default sizes exist

--- frontend/tests/unit/components/Logo.spec.js
+++ frontend/tests/unit/components/Logo.spec.js
@@ -5,8 +5,15 @@ import Logo from '@/components/Logo'
 
 describe('Logo.vue', () => {
   const wrapper = shallowMount(Logo)
+  const defaultHeight = 35
+  const defaultWidth = 50
 
   it('root element is an svg', () => {
     expect(wrapper.is('svg')).to.equal(true)
+  })
+
+  it('has default height of 35pt and default width of 50pt', () => {
+    expect(wrapper.attributes().height).to.equal(`${defaultHeight}pt`)
+    expect(wrapper.attributes().width).to.equal(`${defaultWidth}pt`)
   })
 })
