f9da4d5cf3ae50298e3099eaf1cba7ca8c0a8b53
Prathamesh Shenoy <shenoyprathamesh123@gmail.com>
2019-02-02 16:38:23 -0600

    forgot to add funcs to file path

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -6,7 +6,7 @@ module.exports = {
         data: `
           @import "@/styles/vars/global_vars.scss";
           @import "@/styles/mixins/global_mixins.scss";
-          @import "@/styles/global_funcs.scss";
+          @import "@/styles/funcs/global_funcs.scss";
         `
       }
     }
