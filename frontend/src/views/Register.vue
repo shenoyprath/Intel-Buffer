69931f767e8218dd7bb9a76ad5b4c33fdf1bfb26
Prathamesh Shenoy <shenoyprathamesh123@gmail.com>
2019-02-02 17:58:26 -0600

    updated register view

---
+++ frontend/src/views/Register.vue
@@ -0,0 +1,18 @@
+<template>
+  <modal-authentication-register/>
+</template>
+
+<script>
+import ModalAuthenticationRegister from '@/components/ModalAuthenticationRegister'
+
+export default {
+  name: 'Register',
+  components: {
+    ModalAuthenticationRegister
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
