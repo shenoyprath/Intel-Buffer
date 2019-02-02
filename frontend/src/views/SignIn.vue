75d2ca6211e475d4f1468cee205da75fbdf5f7e2
Prathamesh Shenoy <shenoyprathamesh123@gmail.com>
2019-02-02 17:19:35 -0600

    updated the sign in view

---
+++ frontend/src/views/SignIn.vue
@@ -0,0 +1,18 @@
+<template>
+  <modal-authentication-sign-in/>
+</template>
+
+<script>
+import ModalAuthenticationSignIn from '@/components/ModalAuthenticationSignIn'
+
+export default {
+  name: 'SignIn',
+  components: {
+    ModalAuthenticationSignIn
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
