a382cc4be3161770032d83672be63394b1346348
Prathamesh Shenoy <shenoyprathamesh123@gmail.com>
2019-02-02 21:45:11 -0600

    moved orm instance into models directory

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -4,27 +4,11 @@ import os
 from flask import Flask, Blueprint, current_app, send_file
 from flask_restplus import Api
 
-from peewee import MySQLDatabase
-
 from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
 app.static_folder = os.path.join(app.config["DIST_DIR"], "static")
-
-
-db_user_environ_var = "INTEL_BUFFER_DB_USER"
-db_pass_environ_var = "INTEL_BUFFER_DB_PASS"
-if os.environ.get(db_user_environ_var) is None:
-    raise EnvironmentError(f"Environment variable {db_user_environ_var} isn't set to the username used to access the "
-                           "database.")
-if os.environ.get("INTEL_BUFFER_DB_PASS") is None:
-    raise EnvironmentError(f"Environment variable {db_pass_environ_var} isn't set to the password used to access the "
-                           "database.")
-
-db = MySQLDatabase("intel_buffer_db",
-                   user=os.environ.get("INTEL_BUFFER_DB_USER"),
-                   password=os.environ.get("INTEL_BUFFER_DB_PASS"))
 
 
 # noinspection PyUnusedLocal
---
+++ backend/models/__init__.py
@@ -0,0 +1,17 @@
+import os
+
+from peewee import MySQLDatabase
+
+db_user_environ_var = "INTEL_BUFFER_DB_USER"
+db_pass_environ_var = "INTEL_BUFFER_DB_PASS"
+
+if os.environ.get(db_user_environ_var) is None:
+    raise EnvironmentError(f"Environment variable {db_user_environ_var} isn't set to the username used to access the "
+                           "database.")
+if os.environ.get("INTEL_BUFFER_DB_PASS") is None:
+    raise EnvironmentError(f"Environment variable {db_pass_environ_var} isn't set to the password used to access the "
+                           "database.")
+
+db = MySQLDatabase("intel_buffer_db",
+                   user=os.environ.get("INTEL_BUFFER_DB_USER"),
+                   password=os.environ.get("INTEL_BUFFER_DB_PASS"))
