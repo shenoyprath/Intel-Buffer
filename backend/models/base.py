6a7535e3cc8ba4ab36878a483ad1a2b27f0e2818
Prathamesh Shenoy <shenoyprathamesh123@gmail.com>
2019-02-02 22:11:25 -0600

    instantiate factory method for base model

--- backend/models/base.py
+++ backend/models/base.py
@@ -6,3 +6,17 @@ from models import db
 class Base(Model):
     class Meta:
         database = db
+
+    @classmethod
+    def instantiate(cls, **query):
+        """
+        Factory method for creating instances of models.
+        If complex logic is needed before creating an instance, simply override this method and add the additional
+        functionality before creating the instance.
+
+        Note: This method is used because Peewee advices against overriding `Model.__init__` or `Model.create`.
+        Named 'instantiate' to avoid conflict with `Model.create`.
+        https://github.com/coleifer/peewee/issues/856
+        """
+
+        cls.create(**query)
