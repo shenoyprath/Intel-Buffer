9973b963dda286858a5ecbacc35e749b387cb7d6
Prathamesh Shenoy <shenoyprathamesh123@gmail.com>
2019-02-05 00:16:53 -0600

    added init file for initial setup & runs app

---
+++ backend/__init__.py
@@ -0,0 +1,39 @@
+import os
+import sys
+
+from flask import Flask
+from flask_jwt_extended import JWTManager
+
+from logger import logger
+from config import Config
+
+from index import index
+
+import api
+
+from models import db
+from models.base import Base
+
+
+if sys.version_info < (3, 7):
+    raise RuntimeError("Python version >= 3.7 is required.")
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.static_folder = os.path.join(app.config["DIST_DIR"], "static")
+
+app.add_url_rule(rule="/", view_func=index, defaults={"path": ""})
+app.add_url_rule(rule="/<path>/", view_func=index)
+
+app.register_blueprint(api.blueprint)
+
+JWTManager(app)
+
+with db:
+    db.create_tables(Base.__subclasses__(), safe=True)
+
+
+if __name__ == "__main__":
+    logger()
+    app.run(host="0.0.0.0", port=8888, debug=True)
