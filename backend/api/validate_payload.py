385c0ffcdee2e9bf1fa3df79ee469426a9895934
Prathamesh Shenoy <shenoyprathamesh123@gmail.com>
2019-02-07 00:23:56 -0600

    fail status code when validation fails

--- backend/api/validate_payload.py
+++ backend/api/validate_payload.py
@@ -5,7 +5,7 @@ from marshmallow import ValidationError
 from api import rest_api
 
 
-def validate_payload(schema):
+def validate_payload(schema, fail_status_code):
     def decorator(function):
         def wrapper(*args, **kwargs):
             try:
@@ -13,7 +13,9 @@ def validate_payload(schema):
                 return function(payload=payload.data, *args, **kwargs)
 
             except ValidationError as e:
-                return jsonify(error_messages=e.messages)
+                response = jsonify(error_messages=e.messages)
+                response.status_code = fail_status_code
+                return response
 
         return wrapper
     return decorator
