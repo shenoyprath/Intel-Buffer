a83f8da4505174c03cd9509c0a88f52779a640fb
Prathamesh Shenoy <shenoyprathamesh123@gmail.com>
2019-02-07 22:18:24 -0600

    renamed a var

--- backend/models/user.py
+++ backend/models/user.py
@@ -14,8 +14,8 @@ class User(Base):
 
     email_address = CharField(unique=True)
 
-    password_min_len, password_max_len = 8, 50
-    password = CharField(max_length=password_max_len)
+    min_password_len, max_password_len = 8, 50
+    password = CharField(max_length=max_password_len)
 
     bio = CharField(max_length=160, null=True)  # user's 160 character bio
 
