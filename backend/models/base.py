9715694e4d5b18ca887c6cf7c7b65a94fd326c8a
Prathamesh Shenoy <shenoyprathamesh123@gmail.com>
2019-02-07 22:17:40 -0600

    added a todo for model __repr__

--- backend/models/base.py
+++ backend/models/base.py
@@ -37,5 +37,6 @@ class Base(Model):
         return cls.get_or_none(cls.id == identity)
 
     def __repr__(self):
+        # TODO: change format to "className(key: value, key: value,...)"
         return "\n".join(f"{key}: {value}"
                          for key, value in model_to_dict(self).items())
