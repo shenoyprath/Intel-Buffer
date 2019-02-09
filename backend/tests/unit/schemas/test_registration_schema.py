02d703532d7d2e04054e23f4615b4a66a460ade2
Prathamesh Shenoy <shenoyprathamesh123@gmail.com>
2019-02-09 01:23:37 -0600

    added outline for testing registration schema

---
+++ backend/tests/unit/schemas/test_registration_schema.py
@@ -0,0 +1,32 @@
+class TestRegistrationSchema:
+    @staticmethod
+    def test_invalidates_blank_fields():
+        pass
+
+    @staticmethod
+    def test_invalidates_just_whitespace_names():
+        pass
+
+    @staticmethod
+    def test_invalidates_long_passwords():
+        pass
+
+    @staticmethod
+    def test_invalidates_short_passwords():
+        pass
+
+    @staticmethod
+    def test_invalidates_password_without_letters():
+        pass
+
+    @staticmethod
+    def test_invalidates_password_without_nums():
+        pass
+
+    @staticmethod
+    def test_invalidates_password_matching_email():
+        pass
+
+    @staticmethod
+    def test_validates_when_all_criteria_meet():
+        pass
