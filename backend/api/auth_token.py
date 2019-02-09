b0ead5c0bba49833a71bcbb9d16d0c52c3a46885
Prathamesh Shenoy <shenoyprathamesh123@gmail.com>
2019-02-08 23:17:22 -0600

    method to log the user out

--- backend/api/auth_token.py
+++ backend/api/auth_token.py
@@ -4,11 +4,12 @@ from flask import jsonify
 from werkzeug.security import check_password_hash
 
 from flask_restplus import Resource
-from flask_jwt_extended import create_access_token, create_refresh_token
+from flask_jwt_extended import create_access_token, create_refresh_token, jwt_required, get_jwt_identity, \
+                               get_jwt_claims, get_raw_jwt
 
 from app import jwt
 
-from api import rest_api
+from api import rest_api, redis_db
 
 from models import db
 from models.user import User
@@ -51,3 +52,18 @@ class AuthToken(Resource):
         response = jsonify(error_message="Invalid credentials. Please try again.")
         response.status_code = 401
         return response
+
+    @staticmethod
+    @jwt_required
+    def delete():
+        """
+        Blacklists the client's authentication token till they expire. Used to "log the user out".
+        No way to avoid state management here because token needs to be invalidated immediately.
+        """
+
+        token_expiration = get_jwt_claims().get("creation_timestamp")
+        expiration_delta = datetime.utcnow() - datetime.strptime(token_expiration, "%Y-%m-%d %H:%M:%S.%f")
+
+        redis_db.set(name=f"token_blacklist:{get_jwt_identity()}",
+                     value=get_raw_jwt(),
+                     ex=expiration_delta.total_seconds())
