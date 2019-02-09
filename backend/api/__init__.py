c8b6f1782e46c543f1a9dc12749b89b7c229772a
Prathamesh Shenoy <shenoyprathamesh123@gmail.com>
2019-02-08 20:56:26 -0600

    added connection to redis

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -1,9 +1,24 @@
+import os
+
 from flask import Blueprint
 
 from flask_restplus import Api
 
+from redis import Redis
+
 
 api_blueprint = Blueprint("api", __name__, url_prefix="/api")
 rest_api = Api(api_blueprint)
+
+
+redis_pass_environ_var = "REDIS_DB_PASS"
+if os.environ.get(redis_pass_environ_var) is None:
+    raise RuntimeError(f"Environment variable {redis_pass_environ_var} isn't set to the password used to access the"
+                       f"Redis database")
+
+redis_db = Redis(host="127.0.0.1",
+                 port=6379,
+                 decode_responses=True,
+                 password=os.environ.get(redis_pass_environ_var))
 
 from api import auth_token
