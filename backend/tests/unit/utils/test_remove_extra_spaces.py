8c2215a3fc7feb96cf4e1199e498045429b7ca5b
Prathamesh Shenoy <shenoyprathamesh123@gmail.com>
2019-02-10 17:43:11 -0600

    updated tests to match change in util

--- backend/tests/unit/utils/test_remove_extra_spaces.py
+++ backend/tests/unit/utils/test_remove_extra_spaces.py
@@ -1,15 +1,15 @@
 from hypothesis import given, example
-from hypothesis.strategies import text, iterables
+from hypothesis.strategies import characters, text
 
 from utils.remove_extra_spaces import remove_extra_spaces
 
 
 class TestRemoveExtraSpaces:
-    initial_val_example = "\tsome  string\n"  # notice 2 spaces between the words
+    simple_example = "\tsome  string\n"  # notice 2 spaces between the words
 
     @staticmethod
     @given(string=text())
-    @example(initial_val_example)
+    @example(simple_example)
     def test_strips(string):
         result_val = remove_extra_spaces(string)
         if result_val:
@@ -17,23 +17,11 @@ class TestRemoveExtraSpaces:
             assert not result_val[-1].isspace()
 
     @staticmethod
-    def has_consecutive_spaces(string):
-        for index in range(len(string) - 1):
-            if string[index].isspace() and string[index + 1].isspace():
-                return True
-        return False
-
-    @staticmethod
-    @given(string=text())
-    @example(initial_val_example)
+    @given(string=text(characters(whitelist_categories=["C", "Z"])))
+    @example(simple_example)
     def test_removes_extra_whitespace(string):
-        result_val = remove_extra_spaces(string)
-        assert not TestRemoveExtraSpaces.has_consecutive_spaces(result_val)
-
-    @staticmethod
-    @given(strings=iterables(text()))
-    @example([initial_val_example, "\t\n\r"])
-    def test_removes_extra_whitespace_for_multiple(strings):
-        result_val = remove_extra_spaces(*strings)
-        for val in result_val:
-            assert not TestRemoveExtraSpaces.has_consecutive_spaces(val)
+        result = remove_extra_spaces(string)
+        # check if result has consecutive spaces
+        assert not any(result[index].isspace() and
+                       result[index + 1].isspace()
+                       for index in range(len(result) - 1))
