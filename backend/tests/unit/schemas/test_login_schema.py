7a78a59b15c7ff1b38554dc7d5b1e6be831ec9ce
Prathamesh Shenoy <shenoyprathamesh123@gmail.com>
2019-02-11 18:47:50 -0600

    test validates correct credentials

--- backend/tests/unit/schemas/test_login_schema.py
+++ backend/tests/unit/schemas/test_login_schema.py
@@ -1,9 +1,11 @@
 from hypothesis import given
 from hypothesis.strategies import emails, text
 
-from pytest import raises
+from pytest import raises, fail
 
 from marshmallow import ValidationError
+
+from models.user import User
 
 from schemas.login_schema import LoginSchema
 
@@ -30,3 +32,21 @@ class TestLoginSchema(DatabaseAccessor):
                                 "password": password})
 
         assert LoginSchema.errors["invalid_credentials"] in e.value.messages["_schema"]
+
+    @staticmethod
+    @given(email_address=emails(),
+           password=text())
+    def test_validates_correct_credentials(email_address, password):
+        name = " "  # database will not store null value for first_name and last_name columns
+        test_user = User.instantiate(first_name=name,
+                                     last_name=name,
+                                     email_address=email_address,
+                                     password=password)
+
+        try:
+            LoginSchema().load({"email_address": email_address,
+                                "password": password})
+        except ValidationError:
+            fail("ValidationError was unexpectedly raised.")
+
+        test_user.delete_instance()  # clean up so that IntegrityError does not get raised if same email is used again.
