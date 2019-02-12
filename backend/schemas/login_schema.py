d4e3b369d07c6ee5685d7cf0df9de9b17bb2617a
Prathamesh Shenoy <shenoyprathamesh123@gmail.com>
2019-02-11 18:50:14 -0600

    login schema validation

--- backend/schemas/login_schema.py
+++ backend/schemas/login_schema.py
@@ -1,4 +1,10 @@
+from werkzeug.security import check_password_hash
+
+from marshmallow import validates_schema, ValidationError
 from marshmallow.fields import String
+
+from models import db
+from models.user import User
 
 from schemas.base import Base
 
@@ -11,3 +17,16 @@ class LoginSchema(Base):
 
     errors = {"required": "Your email address and password are required.",
               "invalid_credentials": "Invalid credentials. Please try again."}
+
+    @validates_schema
+    def has_valid_credentials(self, data):
+        try:
+            email_address = data["email_address"]
+            password = data["password"]
+        except (AttributeError, KeyError):
+            raise ValidationError(LoginSchema.errors["required"])
+
+        with db:
+            user = User.retrieve(email_address=email_address)
+            if user is None or not check_password_hash(user.password, password):
+                raise ValidationError(LoginSchema.errors["invalid_credentials"])
