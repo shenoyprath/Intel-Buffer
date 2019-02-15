619c52451b6cc7431fcba68fb5c63231937d3126
Prathamesh Shenoy <shenoyprathamesh123@gmail.com>
2019-02-15 14:03:24 -0600

    moved under class declaration

--- backend/schemas/base.py
+++ backend/schemas/base.py
@@ -2,9 +2,9 @@ from marshmallow import Schema
 from marshmallow.fields import Field
 
 
-Field.default_error_messages["required"] = "This field is required."
-
-
 class Base(Schema):
     class Meta:
         strict = True  # schema validation failure will raise ValidationError. marshmallow 2 doesn't do this by default.
+
+
+Field.default_error_messages["required"] = "This field is required."
