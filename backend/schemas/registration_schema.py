50c16c960049d7df6ed58de8550510b3c556df18
Prathamesh Shenoy <shenoyprathamesh123@gmail.com>
2019-02-15 15:00:40 -0600

    converted err msgs into dict

--- backend/schemas/registration_schema.py
+++ backend/schemas/registration_schema.py
@@ -1,5 +1,5 @@
 from marshmallow import ValidationError, validates, validates_schema
-from marshmallow.fields import String, Email
+from marshmallow.fields import Field, String, Email
 from marshmallow.validate import Length
 
 from models import db
@@ -11,31 +11,28 @@ from utils.is_empty_or_space import is_empty_or_space
 
 
 class RegistrationSchema(Base):
-    # TODO: find a way to change the default error messages for all fields without having to pass arguments repeatedly
-    custom_errors = {"required": "This field is required."}
+    custom_errors = {"email_exists": "Email address already exists.",
+                     "password_len": "Password must be between {min} and {max} characters long.",
+                     "password_req_chars": "Password must contain letters and numbers.",
+                     "password_is_email": "Password cannot be the same as your email address."}
 
-    first_name = String(required=True, error_messages=custom_errors)
+    first_name = String(required=True)
 
-    last_name = String(required=True, error_messages=custom_errors)
+    last_name = String(required=True)
 
-    email_exists_msg = "Email address already exists."
-    email_address = Email(required=True, error_messages=custom_errors)
+    email_address = Email(required=True)
 
     min_password_len, max_password_len = 8, 50
-    password_len_msg = "Password must be between {min} and {max} characters long."
-    password_req_chars_msg = "Password must contain letters and numbers."
-    password_is_email_msg = "Password cannot be the same as your email address."
     password = String(required=True,
-                      error_messages=custom_errors,
                       validate=Length(min=min_password_len,
                                       max=max_password_len,
-                                      error=password_len_msg))
+                                      error=custom_errors["password_len"]))
 
     @validates("email_address")
     def is_unique(self, email_address):
         with db:
             if User.retrieve(email_address=email_address) is not None:
-                raise ValidationError(RegistrationSchema.email_exists_msg)
+                raise ValidationError(RegistrationSchema.custom_errors["email_exists"])
 
     @validates("password")
     def has_letters_and_nums(self, password):
@@ -43,7 +40,7 @@ class RegistrationSchema(Base):
         has_numbers = any(char.isdigit() for char in password)
 
         if not has_letters or not has_numbers:
-            raise ValidationError(RegistrationSchema.password_req_chars_msg)
+            raise ValidationError(RegistrationSchema.custom_errors["password_req_chars"])
 
     @validates_schema
     def names_are_not_spaces(self, data):
@@ -55,11 +52,11 @@ class RegistrationSchema(Base):
                 blank_fields.append(field_name)
 
         if blank_fields:
-            raise ValidationError(RegistrationSchema.custom_errors["required"],
+            raise ValidationError(Field.default_error_messages["required"],
                                   field_names=blank_fields)
 
     @validates_schema
     def password_is_not_email(self, data):
         if data.get("password") == data.get("email_address"):
-            raise ValidationError(message=RegistrationSchema.password_is_email_msg,
+            raise ValidationError(message=RegistrationSchema.custom_errors["password_is_email"],
                                   field_names=["password"])
