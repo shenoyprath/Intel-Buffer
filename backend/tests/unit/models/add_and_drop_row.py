aa2569ca0a2d28c2703c9383abe00fc7323b892e
Prathamesh Shenoy <shenoyprathamesh123@gmail.com>
2019-02-15 23:38:40 -0600

    allow static & dynamic col values

--- backend/tests/unit/models/add_and_drop_row.py
+++ backend/tests/unit/models/add_and_drop_row.py
@@ -1,18 +1,31 @@
 from functools import wraps
 
 
-def add_and_drop_row(model, **column_values):
+def add_and_drop_row(model, **static_column_values):
     """
     Decorator for test cases to auto create model instance, run test case, and then destroy instance.
     Useful when one row should not be affecting another row when running a test.
     (Ex: IntegrityError raised when running test that inserts the same email for two users without deleting the first.)
+
+    Example of decorator being used with hypothesis's @given decorator:
+    @given(some_dynamic_test_value=text())
+    @add_and_drop_row(User, static_row_value1="one", static_row_value2="two")
+    def test_case(some_dynamic_test_value):
+
+    :param model: Model that is instantiated.
+    :param static_column_values: The test case will not focus on these values, but they are required to create the row.
     """
 
     def decorator(test_case):
         @wraps(test_case)
-        def wrapper(*args, **kwargs):
-            row = model.instantiate(**column_values)
-            test_return = test_case(*args, **kwargs)
+        def wrapper(*args, **dynamic_column_values):
+            """
+            :param dynamic_column_values: Dynamically generated (using hypothesis) values. The test case focuses on
+            these values, so they are passed to the model instantiation method and the test case.
+            """
+
+            row = model.instantiate(**static_column_values, **dynamic_column_values)
+            test_return = test_case(*args, **dynamic_column_values)
             row.delete_instance()
             return test_return
 
