4e22a75a62dd877267a6aa70b46b062be4d020fc
Prathamesh Shenoy <shenoyprathamesh123@gmail.com>
2019-02-15 23:55:43 -0600

    pass row to test case instead of col vals

--- backend/tests/unit/models/add_and_drop_row.py
+++ backend/tests/unit/models/add_and_drop_row.py
@@ -10,7 +10,7 @@ def add_and_drop_row(model, **static_column_values):
     Example of decorator being used with hypothesis's @given decorator:
     @given(some_dynamic_test_value=text())
     @add_and_drop_row(User, static_row_value1="one", static_row_value2="two")
-    def test_case(some_dynamic_test_value):
+    def test_case(row):
 
     :param model: Model that is instantiated.
     :param static_column_values: The test case will not focus on these values, but they are required to create the row.
@@ -20,12 +20,11 @@ def add_and_drop_row(model, **static_column_values):
         @wraps(test_case)
         def wrapper(**dynamic_column_values):
             """
-            :param dynamic_column_values: Dynamically generated (using hypothesis) values. The test case focuses on
-            these values, so they are passed to the model instantiation method and the test case.
+            :param dynamic_column_values: Dynamically generated (using hypothesis) column values.
             """
 
             row = model.instantiate(**static_column_values, **dynamic_column_values)
-            test_return = test_case(**dynamic_column_values)
+            test_return = test_case(row=row)
             row.delete_instance()
             return test_return
 
