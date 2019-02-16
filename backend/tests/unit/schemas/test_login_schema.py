0144bebd45ecacde923b9479fa153a2cb4a9bd37
Prathamesh Shenoy <shenoyprathamesh123@gmail.com>
2019-02-16 15:56:16 -0600

    used context manager in test

--- backend/tests/unit/schemas/test_login_schema.py
+++ backend/tests/unit/schemas/test_login_schema.py
@@ -9,7 +9,7 @@ from models.user import User
 
 from schemas.login_schema import LoginSchema
 
-from tests.unit.models.add_and_drop_row import add_and_drop_row
+from tests.unit.models.model_instance import model_instance
 from tests.unit.schemas.get_load_error import get_load_error
 
 
@@ -24,10 +24,14 @@ class TestLoginSchema:
         assert LoginSchema.custom_errors["invalid_credentials"] in e.value.messages["_schema"]
 
     @given(email_address=emails(), password=text())
-    @add_and_drop_row(User, first_name="John", last_name="Doe")  # first & last name are required columns
-    def test_validates_correct_credentials(self, row):
-        try:
-            LoginSchema().load({"email_address": row.email_address,
-                                "password": row.password})
-        except ValidationError:
-            fail("ValidationError was unexpectedly raised.")
+    def test_validates_correct_credentials(self, email_address, password):
+        with model_instance(User,
+                            first_name="John",
+                            last_name="Doe",
+                            email_address=email_address,
+                            password=password):
+            try:
+                LoginSchema().load({"email_address": email_address,
+                                    "password": password})
+            except ValidationError as e:
+                fail(str(e))
