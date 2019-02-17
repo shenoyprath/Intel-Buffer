8288f5fe8b566d665efb2c340128756b8701db0f
Prathamesh Shenoy <shenoyprathamesh123@gmail.com>
2019-02-16 21:01:19 -0600

    clarified use of context manager

--- backend/tests/unit/models/model_instance.py
+++ backend/tests/unit/models/model_instance.py
@@ -7,6 +7,9 @@ def model_instance(model, *instance_args, **instance_kwargs):
     Context manager for use in test cases to auto create model instance, run some test code, and then destroy instance.
     Useful when one row should not be affecting another row when running a test.
     (Ex: IntegrityError raised when running test that inserts the same email for two users without deleting the first.)
+
+    Note: This is used over a pytest fixture as using a parametrized fixture makes the test case every unreadable and
+    hard to integrate with hypothesis's given decorator.
     """
 
     instance = model.instantiate(*instance_args, **instance_kwargs)
