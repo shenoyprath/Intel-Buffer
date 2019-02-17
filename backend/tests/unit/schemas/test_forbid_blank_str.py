f31f388004b4e75268206e86b1d7a8ef19c36899
Prathamesh Shenoy <shenoyprathamesh123@gmail.com>
2019-02-16 20:10:12 -0600

    test ForbidBlankStr invalidates whitespace str

--- backend/tests/unit/schemas/test_forbid_blank_str.py
+++ backend/tests/unit/schemas/test_forbid_blank_str.py
@@ -1,3 +1,8 @@
+from string import whitespace
+
+from hypothesis import given
+from hypothesis.strategies import text, characters
+
 from pytest import raises, fail
 
 from marshmallow import ValidationError
@@ -17,3 +22,10 @@ class TestForbidBlankStr:
             validator(None)
         except ValidationError as e:
             fail(f"ValidationError was unexpectedly raised when validating None: \n{str(e)}")
+
+    @given(string=text(characters(whitelist_categories=(),
+                                  whitelist_characters=list(whitespace))))
+    def test_invalidates_whitespace_str(self, string):
+        validator = ForbidBlankStr(forbid_whitespace_str=True)
+        with raises(ValidationError):
+            validator(string)
