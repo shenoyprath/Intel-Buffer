0ff131ef0c9cbbd622a50b6a36c8c01e06285d05
Prathamesh Shenoy <shenoyprathamesh123@gmail.com>
2019-02-16 20:11:39 -0600

    validator to validate blank strs

--- backend/schemas/forbid_blank_str.py
+++ backend/schemas/forbid_blank_str.py
@@ -1,4 +1,7 @@
-from marshmallow.validate import Validator
+from marshmallow.validate import Validator, ValidationError
+from marshmallow.fields import Field
+
+from utils.is_empty_or_space import is_empty_or_space
 
 
 class ForbidBlankStr(Validator):
@@ -8,10 +11,15 @@ class ForbidBlankStr(Validator):
     """
 
     def __init__(self, forbid_whitespace_str=False, error=None):  # whitespace str only contains whitespace characters
-        pass
+        self.forbid_whitespace_str = forbid_whitespace_str
+        self.error = error or Field.default_error_messages["required"]
 
     def __call__(self, value):
-        pass
+        if value is None:  # if value=None and allow_none=False, required err msg shouldn't duplicate in err msgs dict
+            return
+
+        if not value or (self.forbid_whitespace_str and is_empty_or_space(value)):
+            raise ValidationError(self.error)
 
     def _repr_args(self):
-        pass
+        return f"forbid_whitespace_str={self.forbid_whitespace_str}"
