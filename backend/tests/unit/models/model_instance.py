cbd89347ccaf8d5d31aa87d156c66305bb996977
Prathamesh Shenoy <shenoyprathamesh123@gmail.com>
2019-02-16 20:58:40 -0600

    moved cleanup to finally block

--- backend/tests/unit/models/model_instance.py
+++ backend/tests/unit/models/model_instance.py
@@ -4,11 +4,14 @@ from contextlib import contextmanager
 @contextmanager
 def model_instance(model, *instance_args, **instance_kwargs):
     """
-    Context manager for use in test cases to auto create model instance, run test case, and then destroy instance.
+    Context manager for use in test cases to auto create model instance, run some test code, and then destroy instance.
     Useful when one row should not be affecting another row when running a test.
     (Ex: IntegrityError raised when running test that inserts the same email for two users without deleting the first.)
     """
 
     instance = model.instantiate(*instance_args, **instance_kwargs)
-    yield instance
-    instance.delete_instance()
+
+    try:
+        yield instance
+    finally:  # If cleanup is not included in a 'finally' block, pytest will skip cleanup if test/assertion fails
+        instance.delete_instance()
