310b41281c4a5a46fbe43cfbf82c26d0566d82e8
Prathamesh Shenoy <shenoyprathamesh123@gmail.com>
2019-02-17 20:24:26 -0600

    implemented error_if_undef

--- backend/utils/undef_env_var.py
+++ backend/utils/undef_env_var.py
@@ -1,6 +1,12 @@
+import os
+
+
 class UndefinedEnvironmentVariable(EnvironmentError):
     pass
 
 
 def error_if_undef(env_var):
-    pass
+    if os.getenv(env_var) is None:
+        raise UndefinedEnvironmentVariable(
+            f"Environment variable {env_var} is undefined."
+        )
