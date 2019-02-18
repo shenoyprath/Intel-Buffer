51fcd719ab9351262d466086cfdd6e03558b92ac
Prathamesh Shenoy <shenoyprathamesh123@gmail.com>
2019-02-17 19:17:04 -0600

    forgot to refactor import

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,12 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-import users from './modules/user'
+import user from './modules/user'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
-    users: users
+    user: user
   }
 })
