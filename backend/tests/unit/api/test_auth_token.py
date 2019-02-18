3686607747423fcf8dd452f9c8f1ff80fa4e2200
Prathamesh Shenoy <shenoyprathamesh123@gmail.com>
2019-02-17 21:37:07 -0600

    used dummy instead of generating

--- backend/tests/unit/api/test_auth_token.py
+++ backend/tests/unit/api/test_auth_token.py
@@ -2,30 +2,25 @@ import json
 
 from flask import url_for
 
-from hypothesis import given
-from hypothesis.strategies import emails, text
-
 from models.user import User
 
 from tests.unit.models.model_instance import model_instance
 
 
 class TestAuthToken:
-    @given(
-        email_address=emails(),
-        password=text(min_size=1)
-    )
-    def test_post_returns_access_and_refresh_tokens(self, client, email_address, password):
+    def test_post_returns_access_and_refresh_tokens(self, client):
+        dummy_email = "example@example.com"
+        dummy_password = "Password123"
         with model_instance(
             User,
             first_name="John",
             last_name="Doe",
-            email_address=email_address,
-            password=password
+            email_address=dummy_email,
+            password=dummy_password
         ):
             response = client.post(
                 url_for("api.auth_token"),
-                data=dict(email_address=email_address, password=password)
+                data=dict(email_address=dummy_email, password=dummy_password)
             )
 
         assert response.status_code == 200
