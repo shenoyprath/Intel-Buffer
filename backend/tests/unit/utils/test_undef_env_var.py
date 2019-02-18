f53a61cf4d296c0009f2e8aa7e5b2f9000ff0dd3
Prathamesh Shenoy <shenoyprathamesh123@gmail.com>
2019-02-17 20:23:44 -0600

    test passes def env var

--- backend/tests/unit/utils/test_undef_env_var.py
+++ backend/tests/unit/utils/test_undef_env_var.py
@@ -1,7 +1,10 @@
-from hypothesis import given
-from hypothesis.strategies import text
+import os
+from string import ascii_letters, digits
 
-from pytest import raises
+from hypothesis import given
+from hypothesis.strategies import text, characters
+
+from pytest import raises, fail
 
 from utils.undef_env_var import error_if_undef, UndefinedEnvironmentVariable
 
@@ -11,3 +14,22 @@ class TestUndefEnvVar:
     def test_errors_undef_env_var(self, env_var):
         with raises(UndefinedEnvironmentVariable):
             error_if_undef(env_var)
+
+    @given(env_var=text(
+        characters(
+            whitelist_characters=tuple(ascii_letters + digits),  # avoids encoding & illegal name errors
+            whitelist_categories=()
+        ),
+        min_size=1
+    ))
+    def test_passes_defined_env_var(self, env_var):
+        os.environ[env_var] = env_var
+        try:
+            error_if_undef(env_var)
+        except UndefinedEnvironmentVariable:
+            fail(
+                f"Raised UndefinedEnvironmentVariable exception when "
+                f"environment variable '{env_var}' was defined."
+            )
+        finally:
+            del os.environ[env_var]
