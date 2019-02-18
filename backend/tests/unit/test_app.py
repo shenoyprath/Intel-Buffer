b02838bc71d299b6296dacc41a482eaf4123e93c
Prathamesh Shenoy <shenoyprathamesh123@gmail.com>
2019-02-17 18:07:40 -0600

    test app has all blueprints

--- backend/tests/unit/test_app.py
+++ backend/tests/unit/test_app.py
@@ -1,3 +1,5 @@
 class TestApp:
     def test_has_all_blueprints(self, app):
-        print(app.blueprints)
+        blueprints = "api",
+        for blueprint in blueprints:
+            assert blueprint in app.blueprints
