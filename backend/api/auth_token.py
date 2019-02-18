5df2e9eb21c928d6ad0d9fab721a26463e20b0a2
Prathamesh Shenoy <shenoyprathamesh123@gmail.com>
2019-02-17 21:34:30 -0600

    will implement logout later

--- backend/api/auth_token.py
+++ backend/api/auth_token.py
@@ -4,21 +4,17 @@ from flask import jsonify
 
 from flask_restplus import Resource
 
-from flask_jwt_extended import create_access_token, create_refresh_token, jwt_required, get_jwt_identity, \
-                               get_jwt_claims, get_raw_jwt
+from flask_jwt_extended import create_access_token, create_refresh_token
 
 from webargs.flaskparser import use_args
 
-from api import rest_api, jwt, redis_db
+from api import rest_api, jwt
 
 from schemas.login_schema import LoginSchema
 
 
 @rest_api.route("/auth_token", methods=("POST",))
 class AuthToken(Resource):
-
-    redis_blacklist_namespace = "token_blacklist:"
-
     @staticmethod
     @jwt.user_claims_loader
     def add_token_claims(identity):
@@ -49,30 +45,3 @@ class AuthToken(Resource):
         )
         response.status_code = 200
         return response
-
-    @staticmethod
-    @jwt.token_in_blacklist_loader
-    def is_token_blacklisted(jwt_dict):
-        user_email_address = jwt_dict.get("jti")  # jti stores the identity
-        return redis_db.get(
-            f"{AuthToken.redis_blacklist_namespace}"
-            f"{user_email_address}"
-        ) is not None
-
-    @staticmethod
-    @jwt_required
-    def delete():
-        """
-        Blacklists the client's authentication tokens till it expires. Used to "log the client out".
-        No way to avoid state management here because token needs to be invalidated immediately.
-        """
-
-        token_expiration = get_jwt_claims().get("creation_timestamp")
-        expiration_delta = datetime.utcnow() - datetime.strptime(token_expiration, "%Y-%m-%d %H:%M:%S.%f")
-
-        redis_db.set(
-            name=f"{AuthToken.redis_blacklist_namespace}"
-                 f"{get_jwt_identity()}",
-            value=get_raw_jwt(),
-            ex=expiration_delta.total_seconds()
-        )
