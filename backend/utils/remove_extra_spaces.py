31712c1bf3c74697753ba1e382d3c81dc78caec8
Prathamesh Shenoy <shenoyprathamesh123@gmail.com>
2019-02-19 20:55:21 -0600

    made docstring more clear

--- backend/utils/remove_extra_spaces.py
+++ backend/utils/remove_extra_spaces.py
@@ -1,6 +1,6 @@
 def remove_extra_spaces(string):
     """
-    Substitutes multiple whitespace with one space and strips the strings.
+    Substitutes multiple whitespace with one space and strips the string.
     """
 
     return " ".join(string.split())
