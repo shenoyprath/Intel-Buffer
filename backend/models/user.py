35e1a84574332cfd1371a2578b14ccafd73c2f29
Prathamesh Shenoy <shenoyprathamesh123@gmail.com>
2019-02-19 21:12:59 -0600

    split 2 distinct entities instead of list comprehension

--- backend/models/user.py
+++ backend/models/user.py
@@ -22,7 +22,8 @@ class User(Base):
 
     @classmethod
     def instantiate(cls, first_name, last_name, email_address, password):
-        first_name, last_name = (remove_extra_spaces(name) for name in (first_name, last_name))
+        first_name = remove_extra_spaces(first_name)
+        last_name = remove_extra_spaces(last_name)
         hashed_password = generate_password_hash(password=password, method="sha256")
 
         return cls.create(
