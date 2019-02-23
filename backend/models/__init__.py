c68dc49b7e23b4ba0c9dc73cc0120ff432597ff8
Prathamesh Shenoy <shenoyprathamesh123@gmail.com>
2019-02-23 00:42:26 -0600

    created func to init db

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -5,15 +5,19 @@ from peewee import MySQLDatabase
 from utils.undef_env_var import error_if_undef
 
 
-db_user_environ_var = "INTEL_BUFFER_DB_USER"
-db_pass_environ_var = "INTEL_BUFFER_DB_PASS"
+db = MySQLDatabase(None)
 
-error_if_undef(db_user_environ_var)
-error_if_undef(db_pass_environ_var)
 
-db = MySQLDatabase(
-    "intel_buffer_db",
-    user=os.getenv(db_user_environ_var),
-    password=os.getenv(db_pass_environ_var),
-    charset="utf8mb4"
-)
+def init_db(db_name):
+    user_env_var = "INTEL_BUFFER_DB_USER"
+    pass_env_var = "INTEL_BUFFER_DB_PASS"
+
+    for env_var in (user_env_var, pass_env_var):
+        error_if_undef(env_var)
+
+    db.init(
+        db_name,
+        user=os.getenv(user_env_var),
+        password=os.getenv(pass_env_var),
+        charset="utf8mb4"
+    )
