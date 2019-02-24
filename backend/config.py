a4e73169dfa05d6faa6238200b57e4a7584e6b25
Prathamesh Shenoy <shenoyprathamesh123@gmail.com>
2019-02-23 20:50:33 -0600

    used tuple for immutable list

--- backend/config.py
+++ backend/config.py
@@ -19,7 +19,7 @@ class Config(ABC):
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=15)
     JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=30)
     JWT_BLACKLIST_ENABLED = True
-    JWT_BLACKLIST_TOKEN_CHECKS = ["access", "refresh"]
+    JWT_BLACKLIST_TOKEN_CHECKS = ("access", "refresh")
 
     if not os.path.exists(DIST_DIR):  # pragma: no cover
         raise NotADirectoryError(f"DIST_DIR not found: {DIST_DIR}")
