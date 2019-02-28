07e977d5512976460160ebcaecf585979a9038dd
Prathamesh Shenoy <shenoyprathamesh123@gmail.com>
2019-02-28 13:53:54 -0600

    used get_jti method instead of ['jti']

--- backend/api/auth_token.py
+++ backend/api/auth_token.py
@@ -3,7 +3,7 @@ from time import time
 from flask import jsonify
 
 from flask_restplus import Resource
-from flask_jwt_extended import create_access_token, create_refresh_token, get_raw_jwt, jwt_required
+from flask_jwt_extended import create_access_token, create_refresh_token, get_raw_jwt, jwt_required, get_jti
 from webargs.flaskparser import use_args
 
 from api import rest_api, jwt, redis_db
@@ -41,7 +41,7 @@ class AuthToken(Resource):
     @jwt.token_in_blacklist_loader
     def is_token_revoked(token):
         db_key = AuthToken.get_db_key(token)
-        return redis_db.get(db_key) == token["jti"]
+        return redis_db.get(db_key) == get_jti(token)
 
     @classmethod
     @jwt_required
@@ -56,7 +56,7 @@ class AuthToken(Resource):
         storage_duration = token["exp"] - int(time())
         redis_db.set(
             name=cls.get_db_key(token),
-            value=token["jti"],
+            value=get_jti(token),
             ex=storage_duration
         )
 
