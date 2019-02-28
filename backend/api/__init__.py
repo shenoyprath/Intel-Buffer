410b92fb9c03993882782c6bf981277dc42dc68a
Prathamesh Shenoy <shenoyprathamesh123@gmail.com>
2019-02-27 21:18:44 -0600

    manually set jwt error handlers
    Restplus implements its own error handlers while JWT's error handlers are implemented with flask's native error handlers. Restplus catches the error before the native Flask implementation does and therefore, the error never makes it to Flask JWT. This means that the error messages JWT wants to throw never make it to the client. Here, we use a protected JWT method to add the error handlers manually to the restplus api instance.
    https://github.com/vimalloc/flask-jwt-extended/issues/83
    https://github.com/vimalloc/flask-jwt-extended/issues/86

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -11,7 +11,13 @@ from utils.getenv_or_err import getenv_or_err
 api_blueprint = Blueprint("api", __name__, url_prefix="/api")
 rest_api = Api(api_blueprint)
 jwt = JWTManager()
-
+# Restplus implements its own error handlers while JWT's error handlers are implemented with flask's native error
+# handlers. Restplus catches the error before the native Flask implementation does and therefore, the error never makes
+# it to Flask JWT. This means that the error messages JWT wants to throw never make it to the client. Here, we use a
+# protected JWT method to add the error handlers manually to the restplus api instance.
+# https://github.com/vimalloc/flask-jwt-extended/issues/83 and https://github.com/vimalloc/flask-jwt-extended/issues/86
+# noinspection PyProtectedMember
+jwt._set_error_handler_callbacks(rest_api)
 
 redis_db = Redis(
     host="127.0.0.1",
