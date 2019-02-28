84596766113b057647f19ed1086cf39514c1d6d8
Prathamesh Shenoy <shenoyprathamesh123@gmail.com>
2019-02-27 22:17:35 -0600

    add iat to key & check jti for value
    Only using the email address for the key can create some problems. For example, if the user logs in and logs out immediatelytwice in a row, then the token stored in the redis blacklist the first time the user logged out, will be replaced with the token that the user used the second time. This means that the first token becomes valid and can be used to authenticate. To avoid this problem, the iat is added to the key. Additionally, the blacklist loader not only checks if the key exists in the db, but also if the jti in the token is the same as the jti in the db. This makes such problems less likely.

--- backend/api/auth_token.py
+++ backend/api/auth_token.py
@@ -3,7 +3,7 @@ from time import time
 from flask import jsonify
 
 from flask_restplus import Resource
-from flask_jwt_extended import create_access_token, create_refresh_token, get_jwt_identity, get_raw_jwt, jwt_required
+from flask_jwt_extended import create_access_token, create_refresh_token, get_raw_jwt, jwt_required
 from webargs.flaskparser import use_args
 
 from api import rest_api, jwt, redis_db
@@ -34,37 +34,34 @@ class AuthToken(Resource):
         return jsonify(tokens)
 
     @classmethod
-    def concat_namespace(cls, key):
-        return f"{cls.redis_namespace}:{key}"
+    def get_db_key(cls, token):
+        email_address, token_iat = token["identity"], token["iat"]
+        return (
+            f"{cls.redis_namespace}:{email_address}{token_iat}"
+        )
 
     # making this a class method messes with jwt as it passes one arg without passing the class.
     @staticmethod
     @jwt.token_in_blacklist_loader
     def is_token_blacklisted(token):
-        email_address = token.get("identity")
-        return redis_db.get(
-            AuthToken.concat_namespace(email_address)
-        ) is not None
+        db_key = AuthToken.get_db_key(token)
+        return redis_db.get(db_key) == token["jti"]
 
     @classmethod
     @jwt_required
     def delete(cls):
         """
-        When the user logs out, the token is not immediately invalidated.
-        However, some way to reject these tokens is still necessary.
-        Therefore, the token is added to a redis database until it expires and any token that's in the database is
-        automatically rejected. No way to avoid state management here.
+        No way to avoid state management here because when the user logs out, the token is not immediately invalidated.
+        However, some way to reject these tokens is still necessary. Therefore, the token is added to the redis database
+        until it expires. Any token that's in the database is automatically rejected
         """
 
-        email_address = get_jwt_identity()
-        jwt_id = get_raw_jwt().get("jti")
-
-        expiration_time = get_raw_jwt().get("exp")
-        storage_delta = expiration_time - int(time())
+        token = get_raw_jwt()
+        storage_duration = token["exp"] - int(time())
         redis_db.set(
-            name=cls.concat_namespace(email_address),
-            value=jwt_id,
-            ex=storage_delta
+            name=cls.get_db_key(token),
+            value=token["jti"],
+            ex=storage_duration
         )
 
         response = jsonify(msg="Token successfully invalidated.")
