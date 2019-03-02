b3b4233fa76d07665b448faf5151b515da094509
Prathamesh Shenoy <shenoyprathamesh123@gmail.com>
2019-03-02 15:29:29 -0600

    added test for create_tokens method

--- backend/tests/integration/api/test_auth_token.py
+++ backend/tests/integration/api/test_auth_token.py
@@ -2,7 +2,9 @@ import json
 
 from flask import url_for
 
-from pytest import mark
+from flask_jwt_extended import decode_token
+
+from pytest import mark, fixture
 
 from models.user import User
 
@@ -11,7 +13,8 @@ from tests.utils.model_instance import model_instance
 
 @mark.usefixtures("database")
 class TestAuthToken:
-    def test_post_returns_access_and_refresh_tokens(self, client, valid_user_info):
+    @fixture
+    def post_response(self, client, valid_user_info):
         with model_instance(User, **valid_user_info):
             response = client.post(
                 url_for("api.auth_token"),
@@ -20,8 +23,20 @@ class TestAuthToken:
                     password=valid_user_info["password"]
                 )
             )
+        return response
 
-        assert response.status_code == 200
-        json_response = json.loads(response.data)
+    def test_post_returns_access_and_refresh_tokens(self, post_response):
+        assert post_response.status_code == 200
+        json_response = json.loads(post_response.data)
         assert json_response.get("access_token")
         assert json_response.get("refresh_token")
+
+    def test_created_access_token_has_refresh_token_info_in_claims(self, post_response):
+        json_response = json.loads(post_response.data)
+        access_token = decode_token(json_response["access_token"])
+        claims = access_token["user_claims"]
+
+        refresh_token_key = "refresh_token"
+        assert refresh_token_key in claims
+        assert "jti" in claims[refresh_token_key]
+        assert "exp" in claims[refresh_token_key]
