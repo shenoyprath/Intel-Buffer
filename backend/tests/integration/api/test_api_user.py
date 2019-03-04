c1a550da8a16d94d7f63a391ec0a2e7938a33297
Prathamesh Shenoy <shenoyprathamesh123@gmail.com>
2019-03-03 21:20:29 -0600

    reduced test code duplication & improved readability

--- backend/tests/integration/api/test_api_user.py
+++ backend/tests/integration/api/test_api_user.py
@@ -6,12 +6,12 @@ from pytest import mark
 # file named test_api_user to avoid pytest conflict with test_user from unit/models.
 @mark.usefixtures("database")
 class TestUser:
-    def test_post_returns_auth_tokens(self, client, valid_user_info):
-        response = client.post(
+    def test_post_res_has_access_and_refresh_tokens(self, client, valid_user_info):
+        res = client.post(
             url_for("api.user"),
             data=valid_user_info
         )
 
-        assert response.status_code == 200
-        assert response.json.get("access_token")
-        assert response.json.get("refresh_token")
+        assert res.status_code == 200
+        assert res.json.get("access_token")
+        assert res.json.get("refresh_token")
--- backend/tests/integration/api/test_auth_token.py
+++ backend/tests/integration/api/test_auth_token.py
@@ -4,7 +4,6 @@ from flask_jwt_extended import create_access_token, create_refresh_token, decode
 
 from pytest import mark, fixture, raises
 
-from api import redis_db
 from api.auth_token import AuthToken
 
 from models.user import User
@@ -14,89 +13,87 @@ from tests.utils.model_instance import model_instance
 
 @mark.usefixtures("database")
 class TestAuthToken:
-    @fixture
-    def post_response(self, client, valid_user_info):
-        with model_instance(User, **valid_user_info):
-            response = client.post(
-                url_for("api.auth_token"),
-                data=dict(
-                    email_address=valid_user_info["email_address"],
-                    password=valid_user_info["password"]
-                )
-            )
-        return response
+
+    endpoint = "api.auth_token"
 
     @mark.usefixtures("client")
-    def test_serialization_fails_when_multiple_tokens_of_same_type_provided(self):
+    def test_serialization_fails_for_multiple_tokens_of_same_type(self):
         access_tokens = (
-            create_access_token("token1"),
-            create_access_token("token2")
+            create_access_token("foo"),
+            create_access_token("bar")
         )
         refresh_tokens = (
-            create_refresh_token("token1"),
-            create_refresh_token("token2")
+            create_refresh_token("foo"),
+            create_refresh_token("bar")
         )
 
         for same_tokens in (access_tokens, refresh_tokens):
             with raises(TypeError):
                 AuthToken.serialize(*same_tokens)
 
-    def test_post_returns_access_and_refresh_tokens(self, post_response):
-        assert post_response.status_code == 200
-        assert post_response.json.get("access_token")
-        assert post_response.json.get("refresh_token")
+    @fixture
+    def post_res(self, client, valid_user_info):
+        with model_instance(User, **valid_user_info):
+            res = client.post(
+                url_for(self.endpoint),
+                data=dict(
+                    email_address=valid_user_info["email_address"],
+                    password=valid_user_info["password"]
+                )
+            )
+        return res
+
+    def test_post_res_has_access_and_refresh_tokens(self, post_res):
+        assert post_res.status_code == 200
+        assert post_res.json.get("access_token")
+        assert post_res.json.get("refresh_token")
 
     @staticmethod
-    def verify_refresh_claims(access_token):
-        claims = access_token["user_claims"]
-
-        refresh_token_key = "refresh_token"
-        assert refresh_token_key in claims
-        assert "jti" in claims[refresh_token_key]
-        assert "exp" in claims[refresh_token_key]
-
-    def test_post_created_access_token_has_refresh_token_info_in_claims(self, post_response):
-        access_token = decode_token(post_response.json["access_token"])
-        self.verify_refresh_claims(access_token)
+    def get_auth_header(token):
+        return {"Authorization": f"Bearer {token}"}
 
     @fixture
-    def patch_response(self, client, post_response):
-        refresh_token = post_response.json["refresh_token"]
+    def patch_res(self, client, post_res):
+        refresh_token = post_res.json["refresh_token"]
         return client.patch(
+            url_for(self.endpoint),
+            headers=self.get_auth_header(refresh_token)
+        )
+
+    @mark.usefixtures("redis_database")
+    def test_patch_res_has_access_token_only(self, patch_res):
+        assert patch_res.status_code == 200
+        assert patch_res.json.get("access_token")
+        try:
+            patch_res.json["refresh_token"]
+        except KeyError:
+            pass
+
+    @mark.usefixtures("redis_database")
+    def test_access_tokens_have_refresh_token_info_in_claims(self, post_res, patch_res):
+        for res in (post_res, patch_res):
+            access_token = decode_token(res.json["access_token"])
+            claims = access_token["user_claims"]
+
+            refresh_token_key = "refresh_token"
+            assert refresh_token_key in claims
+            assert "jti" in claims[refresh_token_key]
+            assert "exp" in claims[refresh_token_key]
+
+    @mark.usefixtures("redis_database")
+    def test_delete_invalidates_access_and_refresh_tokens(self, client, post_res):
+        access_token = post_res.json["access_token"]
+        decoded_access_token = decode_token(access_token)
+        decoded_refresh_token = decode_token(post_res.json["refresh_token"])
+
+        for token in (decoded_access_token, decoded_refresh_token):
+            assert not AuthToken.is_token_revoked(token)
+
+        res = client.delete(
             url_for("api.auth_token"),
-            headers={"Authorization": f"Bearer {refresh_token}"}
+            headers=TestAuthToken.get_auth_header(access_token)
         )
+        assert res.status_code == 200
 
-    @mark.usefixtures("redis_database")
-    def test_patch_returns_new_access_token_only(self, patch_response):
-        assert patch_response.status_code == 200
-        assert patch_response.json.get("access_token")
-        assert patch_response.json.get("refresh_token") is None
-
-    @mark.usefixtures("redis_database")
-    def test_patch_created_access_token_has_refresh_token_info_in_claims(self, patch_response):
-        access_token = decode_token(patch_response.json["access_token"])
-        self.verify_refresh_claims(access_token)
-
-    @mark.usefixtures("redis_database")
-    def test_delete_invalidates_access_and_refresh_tokens(self, client, post_response):
-        access_token = post_response.json["access_token"]
-
-        def send_delete_request():
-            return client.delete(
-                url_for("api.auth_token"),
-                headers={"Authorization": f"Bearer {access_token}"}
-            )
-
-        assert send_delete_request().status_code == 200
-
-        # send delete request again with revoked token
-        response = send_delete_request()
-        assert response.status_code == 401
-        assert response.json["msg"] == "Token has been revoked"
-
-        # assert that refresh token is revoked
-        decoded_refresh_token = decode_token(post_response.json["refresh_token"])
-        assert redis_db.get(
-            AuthToken.get_db_key(decoded_refresh_token)
-        )
+        for token in (decoded_access_token, decoded_refresh_token):
+            assert AuthToken.is_token_revoked(token)
