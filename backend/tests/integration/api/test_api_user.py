f122c3b92e55478cf8a41c2b9456aca54d3fad15
Prathamesh Shenoy <shenoyprathamesh123@gmail.com>
2019-03-14 22:56:39 -0500

    test for post method of User

--- backend/tests/integration/api/test_api_user.py
+++ backend/tests/integration/api/test_api_user.py
@@ -1,3 +1,5 @@
+from flask import url_for
+
 from pytest import mark
 
 
@@ -6,3 +8,12 @@ from pytest import mark
 class TestUser:
 
     endpoint = "api.user"
+
+    def test_post_res_has_info_of_newly_register_user(self, client, valid_user_info):
+        post_res = client.post(
+            url_for(self.endpoint),
+            data=valid_user_info
+        )
+        user_info = ("id", "first_name", "last_name")
+        for info in user_info:
+            assert info in post_res.json
