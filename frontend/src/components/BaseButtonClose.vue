f6c562e04a8d5bf3594b08ae8757f3b9df77123f
Prathamesh Shenoy <shenoyprathamesh123@gmail.com>
2019-03-17 00:47:10 -0500

    base component for close btn

---
+++ frontend/src/components/BaseButtonClose.vue
@@ -0,0 +1,45 @@
+<template>
+  <button class="close"></button>
+</template>
+
+<script>
+export default {
+  name: "BaseButtonClose"
+}
+</script>
+
+<style lang="scss" scoped>
+.close {
+  @include grow-on-hover;
+
+  position: relative;
+  display: inline-block;
+
+  width: 50px;
+  height: 50px;
+  overflow: hidden;
+
+  background-color: transparent;
+
+  &::before, &::after {
+    content: '';
+    position: absolute;
+    top: 50%;
+    left: 0;
+
+    height: 1px;
+    width: 100%;
+    margin-top: -1px;
+
+    background-color: $foreground-color;
+  }
+
+  &::before {
+    transform: rotate(45deg);
+  }
+
+  &::after {
+    transform: rotate(-45deg);
+  }
+}
+</style>
