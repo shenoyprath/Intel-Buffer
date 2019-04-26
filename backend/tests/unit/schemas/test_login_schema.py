790cd2e240651e134a08fa3743aae693680a3dc4
Prathamesh Shenoy <shenoyprathamesh123@gmail.com>
2019-04-26 00:03:15 -0500

    added tests for user enum

--- backend/tests/unit/schemas/test_login_schema.py
+++ backend/tests/unit/schemas/test_login_schema.py
@@ -12,14 +12,36 @@ from tests.utils.model_instance import model_instance
 
 @mark.usefixtures("database")
 class TestLoginSchema:
-    @given(email_address=emails(), password=text(min_size=1))
-    def test_invalidates_incorrect_credentials(self, email_address, password):
+    @given(email_address=emails())
+    def test_invalidates_nonexistent_email_address(self, email_address):
         errors = SignInSchema().validate({
             "email_address": email_address,
-            "password": password
+            "password": "Password123"
         })
 
-        assert SignInSchema.custom_errors["invalid_credentials"] in errors["_schema"]
+        assert SignInSchema.custom_errors["nonexistent_email"] in errors["email_address"]
+
+    @given(
+        email_address=emails(),
+        password=text(min_size=1),
+        incorrect_password=text(min_size=1)
+    )
+    def test_invalidates_incorrect_password_for_existing_email_address(
+        self, email_address, password, incorrect_password
+    ):
+        with model_instance(
+            User,
+            first_name="Jane",
+            last_name="Doe",
+            email_address=email_address,
+            password=password
+        ):
+            errors = SignInSchema().validate({
+                "email_address": email_address,
+                "password": password + incorrect_password  # in case both have the same value.
+            })
+
+        assert SignInSchema.custom_errors["incorrect_password"] in errors["password"]
 
     @given(email_address=emails(), password=text(min_size=1))
     def test_validates_correct_credentials_and_loads_user(self, email_address, password):
