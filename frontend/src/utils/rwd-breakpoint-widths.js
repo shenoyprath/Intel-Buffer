0f063f05539407ef890b2e1a3b3cb168151de136
Prathamesh Shenoy <shenoyprathamesh123@gmail.com>
2019-06-15 18:02:25 -0500

    copied rwd breakpoints to js

---
+++ frontend/src/utils/rwd-breakpoint-widths.js
@@ -0,0 +1,17 @@
+export default Object.freeze({
+  zero: 0,
+
+  phoneSmall: 320,
+  phone: 400,
+  phoneWide: 480,
+  phoneTablet: 560,
+
+  tabletSmall: 640,
+  tablet: 768,
+  tabletWide: 1024,
+
+  desktop: 1248,
+  desktopWide: 1440,
+
+  infinity: Number.POSITIVE_INFINITY
+})
