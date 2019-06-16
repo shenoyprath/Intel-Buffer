03e0de4b6ae0a13262b3837bcb584840d05a21ce
Prathamesh Shenoy <shenoyprathamesh123@gmail.com>
2019-06-16 15:56:45 -0500

    deleted useless SFCs
    Deleted some SFCs that were created when the understanding of Vue was poor and when requirements were not clear.

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,41 +1,9 @@
 <template>
-  <div class="home">
-    <the-splash-screen
-      class="animated fadeOut slow delay-2s"
-      v-if="isSplashVisible"
-    />
-
-    <div
-      class="animated fadeIn"
-      v-else
-    >
-      <navbar/>
-    </div>
-  </div>
+  <div class="home"></div>
 </template>
 
 <script>
-import TheSplashScreen from "@/components/TheSplashScreen"
-import Navbar from "../components/Navbar"
-
 export default {
-  name: "home",
-
-  components: {
-    "the-splash-screen": TheSplashScreen,
-    "navbar": Navbar
-  },
-
-  data () {
-    return {
-      isSplashVisible: true
-    }
-  },
-
-  mounted () {
-    setTimeout(() => {
-      this.isSplashVisible = false
-    }, 4000)
-  }
+  name: "Home"
 }
 </script>
