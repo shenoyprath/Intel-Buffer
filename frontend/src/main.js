82e2fa95fee6fc6ede6d072bd5f81651c1a7b9db
Prathamesh Shenoy <shenoyprathamesh123@gmail.com>
2019-06-17 17:32:08 -0500

    used absolute imports

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,7 @@
 import Vue from "vue"
-import App from "./App.vue"
-import router from "./router"
-import store from "./store/index"
+import App from "@/App.vue"
+import router from "@/router"
+import store from "@/store/index"
 
 // noinspection JSUnusedGlobalSymbols
 new Vue({
