429bd9fe006ab39485539a5fbd129ef45c00039a
Prathamesh Shenoy <shenoyprathamesh123@gmail.com>
2019-07-07 17:59:24 -0500

    created auth modal footnote component

---
+++ frontend/src/components/ModalAuthenticationFootnote.vue
@@ -0,0 +1,17 @@
+<template>
+  <p>
+    <small>
+      <slot/>
+    </small>
+  </p>
+</template>
+
+<script>
+export default {
+  name: "ModalAuthenticationFootnote"
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
